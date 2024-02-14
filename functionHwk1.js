b7bdb68d4891a7782df72d589514c38c1fd9d674
emenansari <123516469+emenansari@users.noreply.github.com>
2024-02-14 02:58:23 -0800

    Add files via upload

---
+++ functionHwk1.js
@@ -0,0 +1,46 @@
+function add(a, b) {
+  return a + b;
+}
+
+const result = add(3, 4);
+console.log(result);
+
+/////////////////////////
+
+function divide(a, b) {
+  if (b !== 0) {
+    return a / b;
+  }
+}
+const output = divide(4, 2);
+console.log(output);
+
+///////////////////////////
+
+function convert(fahrenheit) {
+  const celsius = ((fahrenheit - 32) * 5) / 9;
+  return celsius;
+}
+
+///////////////////////////////////
+
+function divisibleBy55(number) {
+  return number % 55 === 0;
+}
+console.log(divisibleBy55(110)); // true
+console.log(divisibleBy55(70)); // false
+
+//////////////////////////////////////
+
+function checkLength(string) {
+  return string.length;
+}
+console.log(checkLength(homework));
+
+/////////////////////////////////////
+
+function concat(string1, string2) {
+  return string1 + string2;
+}
+const result = contact("cat", "dog");
+console.log(result);
