5b7c52f7b0832cb4afb43073c1af866a63b014f8
emenansari <123516469+emenansari@users.noreply.github.com>
2024-02-16 11:56:48 -0800

    Add files via upload

---
+++ arrayMethodshwk3.js
@@ -0,0 +1,15 @@
+// 1.
+function age(number) {
+  if (age >= 65) {
+    return "Special Discount";
+  } else {
+    return "Not Allowed";
+  }
+}
+
+// 2.
+const newArrc = arr.map((element) => {
+  return element * 100;
+});
+
+// 3.
