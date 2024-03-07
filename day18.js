faba69d2c73a9216b341f1988e5ce2f9f2399102
emenansari <123516469+emenansari@users.noreply.github.com>
2024-03-07 13:42:17 -0800

    Add files via upload

---
+++ day18.js
@@ -0,0 +1,17 @@
+// Write a summary of how maps are implemented? Make sure to address:
+// 1. How do we compute the index from the key (string)?
+// => By using the hash coding method
+
+// 2. Why do we use the modulo operator % to get the hash index?
+// => To find its range
+
+// 3. What do we do with the hash index once we compute it?
+// => Look at the data given
+
+// 4. What is the benefit of using a map over an array?
+// => Map is better when using complex structures and is more flexible
+
+// 5. What are the drawbacks or constraints of using a map versus an array to store information?
+// => Map has lower storage
+
+// 6. Implement a method has(key) for our MockMap data structure. The function should check if the key exists in the map or not. It should return true if the key exist and false if it doesn't.
