6be22c9931621a9440c260789dac2eb48bd4cd72
emenansari <123516469+emenansari@users.noreply.github.com>
2024-03-12 14:13:46 -0700

    Add files via upload

--- OOP3.js
+++ OOP3.js
@@ -1,68 +1,136 @@
+//- encapsulation :
+//- inheritance: uses extend so subclass has same properties as superclass
+//- polymorphism
+
 1; //
 class Book {
   title;
   author;
   yearPublished;
+
+  constructor(title, author, yearPublished) {
+    this.title = title;
+    this.author = author;
+    this.yearPublished = yearPublished;
+  }
+
+  getsummary() {
+    return `${this.title} is written by ${this.author} and published in ${yearPublished}`;
+  }
 }
 
-constructor(title, author, yearPublished);
-this.title = title;
-this.author = author;
-this.yearPublished = yearPublished;
-
-getSummary = function () {
-  return this.title, this.author, yearPublished;
-};
+const ulysses = new Book("Uslysses", "james joyce", "1922");
+// console.log(ulysses.getsummary());
 
 // 2.
 class Recatangle {
   width;
   height;
+
+  constructor(width, height) {
+    this.width = w;
+    this.height = h;
+  }
+
+  calculateArea() {
+    return this.width * this.height;
+  }
+
+  calculatePerimeter() {
+    return this.width * 2 + this.height * 2;
+  }
 }
-constructor(width, height);
-this.width = width;
-this.author = author;
-this.yearPublished = yearPublished;
-
-calculateArea.prototype.getcalculateArea = function () {
-  return this.calculateArea;
-};
-
-calculatePerimeter.prototype.getcalculatePerimeter = function () {
-  return this.calculatePerimeter;
-};
 
 // 3.
 class Vechile {
   make;
   model;
   year;
+
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  getInfo() {
+    return `${this.make}, is for ${this.model}, made in ${this.year}`;
+  }
 }
-constructor(make, model, year);
-this.make = make;
-this.model = model;
-this.year = year;
-
-getInfo = function () {
-  return this.make, this.model, this.year;
-};
-
 class Car extends vehicle {
   constructor(make, model, year, numDoors) {
     super(make, model, year);
     this.numDoors = numDoors;
   }
 
-  getInfo = function () {
-    return super.getinfo;
-  };
+  getInfo() {
+    return `${super.getinfo}, amount of ${this.numDoors}`;
+  }
 }
 class Truck extends Vehicle {
   construction(make, model, year, loadCapicity) {
     super(make, model, year);
     this.loadCapicity = loadCapicity;
   }
-  getInfo() {
-    return super.getInfo(this.loadCapicity);
+}
+  getInfo(){
+    return `${super.getInfo}, ${this.loadCapicity}`;
+  }
+
+  // 4. 
+
+  class Employee {
+    #name
+    #id
+    #email
+     
+    constructor(name,id, email) {
+      this.#name = name;
+      this.#id = id;
+      this.#email = email;
+    }
+  
+  getName () {
+    return this.#name;
+  }
+
+  getId () {
+    return this.#id;
+  }
+
+  getEmail (){
+    return this.#email;
+  }
+  } 
+
+  class Crewmember extends Employee {
+    #manager;
+     constructor(name,id, email, manager) {
+      super (name, id, email);
+      this.#manager = manager
+     }
+  
+  getManager() {
+    return this.#manager;
+  }
+
+  setManager(manager) {
+    this.#manager = manager;
   }
 }
+ class Manager extends Employee {
+  #subordinates;
+  constructor(name, id, email){
+    super(name, id, email);
+    this.#subordinates = subordinates
+  }
+  getSubordinates () {
+    return this.#subordinates
+  }
+  addSubordinates (employee) {
+    this.#subordinates.push(employee);
+  }
+  removeSubordinates(employee) {
+
+  }
+ }
